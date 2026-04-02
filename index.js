3061561d82070fd5872d64bb2b67423b0a13a711
Navish Kumar <smartdata.nav@gmail.com>
2026-04-02 10:17:05 +0530

    add playback recordings

--- index.js
+++ index.js
@@ -754,8 +754,8 @@ const tcpServer = net.createServer(socket => {
 
                         // ← NEW: Query recordings from last 1 hour
                         const now   = new Date();
-                        const start = new Date(now - 60 * 60 * 1000);
-                        socket.write(buildQueryResourceList(phone, 0, start, now)); // 0 = all channels
+                        const start = new Date(now - 24 * 60 * 60 * 1000); // 24 hours ago
+                        socket.write(buildQueryResourceList(phone, 0, start, now));
 
                     } else if (msgId === 0x1205) {
                         // ← NEW: Resource list response
